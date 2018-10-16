05495977ece1b19494b32e1aa1a10c866d161357
ags1773 <amogh1773@gmail.com>
2018-10-16 10:59:02 +0530

    added searchbox

--- src/Components/app.js
+++ src/Components/app.js
@@ -1,12 +1,43 @@
-import React, {Component} from 'react'
+import React, {Component, Fragment} from 'react'
+import InputBox from './inputBox'
+
+const script = `https://maps.googleapis.com/maps/api/js?key=${process.env.API_KEY}&libraries=places`
+function loadScript (src) {
+  return new Promise((resolve, reject) => {
+    let script = document.createElement('script')
+    script.src = src
+    script.addEventListener('load', () => resolve())
+    script.addEventListener('error', e => reject(e))
+    document.body.appendChild(script)
+  })
+}
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      scriptLoaded: false
+    }
+  }
+  componentDidMount () {
+    loadScript(script)
+      .then(() => this.setState({scriptLoaded: true}))
+      .catch(e => console.log(e))
+  }
   render () {
-    return (
-      <div>
-        It works!
-      </div>
-    )
+    if (this.state.scriptLoaded) {
+      return (
+        <Fragment>
+          <InputBox />
+        </Fragment>
+      )
+    } else {
+      return (
+        <Fragment>
+          <h2>Could not load google script</h2>
+        </Fragment>
+      )
+    }
   }
 }
 
