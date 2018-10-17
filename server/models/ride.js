454ead67e7abcdc719b13f381a2a8d8b7da64a89
ekanshiag <ekanshi001@gmail.com>
2018-10-17 12:45:11 +0530

    Add model for rider,driver and ride

---
+++ server/models/ride.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const rideSchema = mongoose.Schema({
+  _id: mongoose.SchemaTypes.ObjectId,
+  origin: {type: {type: String},
+    coordinates: [Number]
+  },
+  destination: {type: {type: String},
+    coordinates: [Number]
+  },
+  rider: {type: mongoose.SchemaTypes.ObjectId, ref: 'Rider'},
+  driver: {type: mongoose.SchemaTypes.ObjectId, ref: 'Driver'},
+  status: String
+})
+
+module.exports = mongoose.Model('Ride', rideSchema)
--- server/server.js
+++ server/server.js
@@ -3,4 +3,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT
 
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+
 app.listen(port)
+
+app.use(morgan('dev'))
+app.use(bodyParser.json())
