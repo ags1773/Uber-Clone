d820b4a5789fea369957d60b2b46458cc3ea8391
ekanshiag <ekanshi001@gmail.com>
2018-10-18 12:07:19 +0530

    Change path for finding drivers

--- server/routes/driver.js
+++ server/routes/driver.js
@@ -2,9 +2,9 @@ const express = require('express')
 const router = express.Router()
 const driverController = require('../controllers/driver')
 
-router.post('/', driverController.findDrivers)
-// router.post('/', driverController.createDriver)
+router.post('/', driverController.createDriver)
 router.patch('/:id', driverController.updateDriver)
 router.delete('/:id', driverController.deleteDriver)
+router.post('/find', driverController.findDrivers)
 
 module.exports = router
