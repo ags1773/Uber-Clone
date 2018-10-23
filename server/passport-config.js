405fa5cb0069c1dfd46b89ae32737d5ebdb5e14d
ekanshiag <ekanshi001@gmail.com>
2018-10-23 18:07:00 +0530

    Create new user only when it does not exist

--- server/passport-config.js
+++ server/passport-config.js
@@ -8,16 +8,20 @@ passport.use(new GoogleStrategy({
   clientSecret: process.env.googleClientSecret,
   callbackURL: '/api/user/login/redirect'
 }, (accessToken, refreshToken, profile, done) => {
-  let newRider = new Users({
-    _id: new mongoose.Types.ObjectId(),
-    name: profile.displayName,
-    googleId: profile.id
-  })
-  newRider.save()
-    .then(result => {
-      console.log(result)
-    })
-    .catch(err => {
-      console.log(err)
+  Users.findOne({googleId: profile.id})
+    .then(user => {
+      if (user) {
+        done(null, user)
+      } else {
+        let newRider = new Users({
+          _id: new mongoose.Types.ObjectId(),
+          name: profile.displayName,
+          googleId: profile.id
+        })
+        newRider.save()
+          .then(result => {
+            done(null, result)
+          })
+      }
     })
 }))
