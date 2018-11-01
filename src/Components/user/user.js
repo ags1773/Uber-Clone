0394ad977d84e0a069a8d68965d0ae20a32e57b5
ags1773 <amogh1773@gmail.com>
2018-11-01 19:29:14 +0530

    created userHome component

--- src/Components/user/user.js
+++ src/Components/user/user.js
@@ -1,39 +1,12 @@
-import React, {Component, Fragment} from 'react'
-import InputBoxes from '../inputBoxes'
-import Map from '../map/map'
-
-let watchId
-let socket, userID
-let options = {
-  enableHighAccuracy: true
-}
-let geoError = () => {
-  console.log('No position available')
-}
+import React, {Component} from 'react'
+import UserHome from './userHome/userHome'
 
 class User extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      user: {},
-      origin: {},
-      destination: {},
-      userPos: {lat: 12.9716, lng: 77.5946},
-      drivers: []
+      status: 'renderHome'
     }
-    socket = this.props.socket
-    userID = this.props.userID
-    socket.emit('userType', 'user', userID)
-  }
-  // ---- Functions ----
-  geoSuccess (position) {
-    this.setState({userPos: {lat: position.coords.latitude, lng: position.coords.longitude}})
-  }
-  updateOriginDestination (obj) {
-    this.setState({
-      origin: obj.origin,
-      destination: obj.destination
-    })
   }
 
   // ---- Lifecycle Hooks ----
@@ -49,62 +22,16 @@ class User extends Component {
       .catch(err => {
         console.log('ERROR ', err)
       })
-    watchId = navigator.geolocation.getCurrentPosition(this.geoSuccess.bind(this), geoError, options)
-  }
-  componentWillUnmount () {
-    navigator.geolocation.clearWatch(watchId)
-  }
-  componentDidMount () {
-    let data = {
-      'userLoc': [this.state.userPos.lng, this.state.userPos.lat]
-    }
-    let myInit = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    }
-    fetch('/api/user/findDrivers', myInit)
-      .then(result => {
-        return result.json()
-      })
-      .then(drivers => {
-        this.setState({drivers: drivers})
-      })
-      .catch(err => {
-        console.log(err.json())
-      })
-  }
-
-  findRide () {
-    if (this.state.drivers.length && this.state.userPos.lat && this.state.userPos.lng && this.state.drivers && this.state.destination) {
-      const payload = {
-        drivers: this.state.drivers,
-        origin: this.state.origin,
-        destination: this.state.destination,
-        userPosition: this.state.userPos
-      }
-      socket.emit('findRide', payload)
-    } else {
-      console.log(`Can't find ride right now`)
-    }
   }
 
   render () {
-    return (
-      <Fragment>
-        <InputBoxes
-          updateOriginDestination={this.updateOriginDestination.bind(this)}
-        />
-        <button onClick={this.findRide.bind(this)}>Find Ride</button>
-        <Map
-          origin={this.state.origin}
-          destination={this.state.destination}
-          userPos={this.state.userPos}
-          drivers={this.state.drivers} />
-      </Fragment>
-    )
+    let component
+    switch (this.state.status) {
+      case 'renderHome':
+        component = <UserHome socket={this.props.socket} />
+        break
+    }
+    return component
   }
 }
 
---
+++ src/Components/user/userHome/userHome.js
@@ -0,0 +1,99 @@
+import React, {Component, Fragment} from 'react'
+import InputBoxes from '../../inputBoxes'
+import Map from '../../map/map'
+
+let watchId
+let socket
+let options = {
+  enableHighAccuracy: true
+}
+let geoError = () => {
+  console.log('No position available')
+}
+
+class User extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      user: {},
+      origin: {},
+      destination: {},
+      userPos: {lat: 12.9716, lng: 77.5946},
+      drivers: []
+    }
+    socket = this.props.socket
+  }
+  // ---- Functions ----
+  geoSuccess (position) {
+    this.setState({userPos: {lat: position.coords.latitude, lng: position.coords.longitude}})
+  }
+  updateOriginDestination (obj) {
+    this.setState({
+      origin: obj.origin,
+      destination: obj.destination
+    })
+  }
+
+  // ---- Lifecycle Hooks ----
+  componentWillMount () {
+    watchId = navigator.geolocation.getCurrentPosition(this.geoSuccess.bind(this), geoError, options)
+    // watchId = navigator.geolocation.watchPosition(this.geoSuccess.bind(this), geoError, options)
+  }
+  componentWillUnmount () {
+    navigator.geolocation.clearWatch(watchId)
+  }
+  componentDidMount () {
+    let data = {
+      'userLoc': [this.state.userPos.lng, this.state.userPos.lat]
+    }
+    let myInit = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }
+    fetch('/api/user/findDrivers', myInit)
+      .then(result => {
+        return result.json()
+      })
+      .then(drivers => {
+        this.setState({drivers: drivers})
+      })
+      .catch(err => {
+        console.log(err.json())
+      })
+  }
+
+  findRide () {
+    if (this.state.drivers.length && this.state.userPos.lat && this.state.userPos.lng && this.state.drivers && this.state.destination) {
+      const payload = {
+        drivers: this.state.drivers,
+        origin: this.state.origin,
+        destination: this.state.destination,
+        userPosition: this.state.userPos
+      }
+      socket.emit('findRide', payload)
+    } else {
+      console.log(`Can't find ride right now`)
+    }
+  }
+
+  render () {
+    return (
+      <Fragment>
+        <InputBoxes
+          updateOriginDestination={this.updateOriginDestination.bind(this)}
+        />
+        <button onClick={this.findRide.bind(this)}>Find Ride</button>
+        <Map
+          origin={this.state.origin}
+          destination={this.state.destination}
+          userPos={this.state.userPos}
+          drivers={this.state.drivers} />
+      </Fragment>
+    )
+  }
+}
+
+export default User
