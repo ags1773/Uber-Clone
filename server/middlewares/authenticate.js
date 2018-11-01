a4e5aad8d0e046352fe1520fc5d6bfea573b725f
ekanshiag <ekanshi001@gmail.com>
2018-11-01 13:07:12 +0530

    Redirect unauthorized user/driver to login page

--- server/middlewares/authenticate.js
+++ server/middlewares/authenticate.js
@@ -1,13 +1,13 @@
 exports.authenticateUser = (req, res, next) => {
   if (!req.session.user) {
-    return res.status(401).send('Please login to access this route!')
+    return res.status(401).redirect('/')
   }
   next()
 }
 
 exports.authenticateDriver = (req, res, next) => {
   if (!req.session.driver) {
-    return res.status(401).send('Please login to access this route!')
+    return res.status(401).redirect('/')
   }
   next()
 }
